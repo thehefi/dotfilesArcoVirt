6b1772b18f8793d97c5eaa0461afbf515415ec90
Helmut Fischer <thehefi@gmail.com>
2021-08-02 18:26:19 +0200

    alias in fisch config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# HeFi 2021-08-02 18:30
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
